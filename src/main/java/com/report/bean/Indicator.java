65d89d6ae7b4ec620a10f4bb0fb7c055ea4ba542
shuhamyedage <shubham.yedage@synerzip.com>
2017-10-09 18:27:15 +0530

    Created bean collector for assets and liabilities structure table.

--- src/main/java/com/report/bean/Indicator.java
+++ src/main/java/com/report/bean/Indicator.java
@@ -2,6 +2,15 @@ package com.report.bean;
 
 public class Indicator {
   private String name;
+  private Double value1;
+  private Double value2;
+  private Double value3;
+  private Double value4;
+  private Double percentageValueBeginning;
+  private Double percentageValueEnd;
+  private Double change;
+  private Double percentageChange;
+  private String description;
 
   public String getName() {
     return name;
@@ -9,5 +18,77 @@ public class Indicator {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Double getValue1() {
+    return value1;
+  }
+
+  public void setValue1(Double value1) {
+    this.value1 = value1;
+  }
+
+  public Double getValue2() {
+    return value2;
+  }
+
+  public void setValue2(Double value2) {
+    this.value2 = value2;
+  }
+
+  public Double getValue3() {
+    return value3;
+  }
+
+  public void setValue3(Double value3) {
+    this.value3 = value3;
+  }
+
+  public Double getValue4() {
+    return value4;
+  }
+
+  public void setValue4(Double value4) {
+    this.value4 = value4;
+  }
+
+  public Double getPercentageValueBeginning() {
+    return percentageValueBeginning;
+  }
+
+  public void setPercentageValueBeginning(Double percentageValueBeginning) {
+    this.percentageValueBeginning = percentageValueBeginning;
+  }
+
+  public Double getPercentageValueEnd() {
+    return percentageValueEnd;
+  }
+
+  public void setPercentageValueEnd(Double percentageValueEnd) {
+    this.percentageValueEnd = percentageValueEnd;
+  }
+
+  public Double getChange() {
+    return change;
+  }
+
+  public void setChange(Double change) {
+    this.change = change;
+  }
+
+  public Double getPercentageChange() {
+    return percentageChange;
+  }
+
+  public void setPercentageChange(Double percentageChange) {
+    this.percentageChange = percentageChange;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
   }
 }
--- src/main/java/com/report/bean_collector/ReportBeanCollector.java
+++ src/main/java/com/report/bean_collector/ReportBeanCollector.java
@@ -2,7 +2,6 @@ package com.report.bean_collector;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.report.bean.Indicator;
 import com.report.bean.ReportText;
 import com.report.config.FileConfig;
@@ -25,12 +24,13 @@ public class ReportBeanCollector {
 
   // Table data collector for Structure of the Assets and Liabilities Part 1.
   public static List<Indicator> getAssetsAndLiabilitiesStructureTable1() {
-    // List<String> data = FileConfig.readCsvFile("report/data/table/amat1.1_table1.csv");
-    // List<Indicator> tableDataSet = new ArrayList<Indicator>();
+    List<String> data = FileConfig.readCsvFile("report/data/table/amat1.1_table1.csv");
+    List<Indicator> indicators = new ArrayList<Indicator>();
     // for (Object obj : data) {
     // String str = obj.toString();
     // String[] rowData = str.split(",");
-    // Indicator row = new Indicator();
+    // Indicator indicator = new Indicator();
+    // indicator.setName("Hii");
     // row.setName(rowData[0]);
     // row.setValue1(Double.parseDouble(rowData[1]));
     // row.setValue2(Double.parseDouble(rowData[2]));
@@ -40,14 +40,70 @@ public class ReportBeanCollector {
     // row.setPercentageValueEnd(Double.parseDouble(rowData[6]));
     // row.setChange(Double.parseDouble(rowData[7]));
     // row.setPercentageChange(Double.parseDouble(rowData[8]));
-    // System.out.println(row.toString());
-    // tableDataSet.add(row);
+    // indicators.add(ind);
+    // indicators.add(indicator);
     // }
-    // return tableDataSet;
-    List<Indicator> indicators = new ArrayList<Indicator>();
     Indicator indicator = new Indicator();
-    indicator.setName("Assets");
+    indicator.setName("1.Non current assets");
+    indicator.setValue1(6207.00);
+    indicator.setValue2(6047.00);
+    indicator.setValue3(6235.00);
+    indicator.setValue4(6347.00);
+    indicator.setPercentageValueBeginning(47.12);
+    indicator.setPercentageValueEnd(33.27);
+    indicator.setChange(140.00);
+    indicator.setPercentageChange(2.26);
     indicators.add(indicator);
+
+    Indicator indicator1 = new Indicator();
+    indicator1.setName("2.Current Assets total");
+    indicator1.setValue1(6967.00);
+    indicator1.setValue2(9261.00);
+    indicator1.setValue3(8353.00);
+    indicator1.setValue4(12731.00);
+    indicator1.setPercentageValueBeginning(52.88);
+    indicator1.setPercentageValueEnd(66.73);
+    indicator1.setChange(5764.00);
+    indicator1.setPercentageChange(82.73);
+    indicators.add(indicator1);
+
+    Indicator indicator2 = new Indicator();
+    indicator2.setName("Inventory");
+    indicator2.setValue1(1567.00);
+    indicator2.setValue2(1833.00);
+    indicator2.setValue3(2050.00);
+    indicator2.setValue4(2876.00);
+    indicator2.setPercentageValueBeginning(11.89);
+    indicator2.setPercentageValueEnd(15.07);
+    indicator2.setChange(1309.00);
+    indicator2.setPercentageChange(83.54);
+    indicators.add(indicator2);
+    
+    Indicator indicator3 = new Indicator();
+    indicator3.setName("Trade and other current receivables");
+    indicator3.setValue1(1670.00);
+    indicator3.setValue2(1739.00);
+    indicator3.setValue3(2279.00);
+    indicator3.setValue4(2258.00);
+    indicator3.setPercentageValueBeginning(12.68);
+    indicator3.setPercentageValueEnd(11.84);
+    indicator3.setChange(588.00);
+    indicator3.setPercentageChange(35.21);
+    indicators.add(indicator3);
+    
+    Indicator indicator4 = new Indicator();
+    indicator4.setName("Cash and cash equivalents");
+    indicator4.setValue1(3002.00);
+    indicator4.setValue2(4797.00);
+    indicator4.setValue3(3406.00);
+    indicator4.setValue4(5278.00);
+    indicator4.setPercentageValueBeginning(22.79);
+    indicator4.setPercentageValueEnd(27.67);
+    indicator4.setChange(2276.00);
+    indicator4.setPercentageChange(75.82);
+    indicators.add(indicator4);
+
+    System.out.println(indicators.toString());
     return indicators;
   }
 
@@ -165,7 +221,7 @@ public class ReportBeanCollector {
 
   public static void main(String[] args) {
     getReportText();
-    // getAssetsAndLiabilitiesStructureTable1();
+    getAssetsAndLiabilitiesStructureTable1();
     // getAssetsAndLiabilitiesStructureTable2();
     // getAssetsAndLiabilitiesStructureTable3();
     // getNetAssetsTable();
